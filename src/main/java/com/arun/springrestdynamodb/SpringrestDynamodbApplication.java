45f125d5ff1593ac7a906c8e2792f9d998907fde
Arun Singh <arun@Aruns-MacBook-Pro.local>
2020-08-05 05:37:02 -0700

    initial commit

---
+++ src/main/java/com/arun/springrestdynamodb/SpringrestDynamodbApplication.java
@@ -0,0 +1,13 @@
+package com.arun.springrestdynamodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringrestDynamodbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringrestDynamodbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/arun/springrestdynamodb/SpringrestDynamodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arun.springrestdynamodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringrestDynamodbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
